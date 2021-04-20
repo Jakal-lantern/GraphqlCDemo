86c35fb89f92701cab4d62e2efde7eec707e761d
Jakal-lantern <57189817+Jakal-lantern@users.noreply.github.com>
2021-04-20 09:25:58 -0400

    Add files via upload

---
+++ Query.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Query
+    {
+        public Character GetCharacter() =>
+           new Character
+           {
+               Name = "Jak",
+               Age = 15,
+               OrbCount = 845,
+               CellCount = 76
+           };
+    }
+}
